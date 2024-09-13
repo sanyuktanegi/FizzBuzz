a54084ed31f34075a0102ad88b8f3bf221f8c46e
sanyuktanegi <sanyuktaa05@gmail.com>
2024-09-13 14:20:24 -0400

    Wrote main method that prints how many steps it takes to reach 0 starting at 100

---
+++ src/Reduce.java
@@ -0,0 +1,11 @@
+public class Reduce {
+    public static void main(String[] args) {
+    int start = 100;
+    int steps = 0;
+    while(start > 0){
+        steps++;
+        start--;
+    }
+    System.out.println(steps);
+    }
+}
